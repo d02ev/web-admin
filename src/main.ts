b3261b5a69dbbe49181e5122853bea67fc61173b
d02ev <vikramadityapratapsingh98@gmail.com>
2023-03-24 12:29:22 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as ExpressSession from 'express-session';
+import * as Passport from 'passport';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = new ConfigService();
+
+  app.use(
+    ExpressSession({
+      secret: configService.get('SESSION_SECRET'),
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 24 * 60 * 60 * 1000, secure: false, httpOnly: true },
+    }),
+  );
+  app.use(Passport.initialize());
+  app.use(Passport.session());
+
+  app.setGlobalPrefix('api/v1');
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
