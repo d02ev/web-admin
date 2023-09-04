7b48f313186e4eb2f3800410d7c6ed51cdb81eee
d02ev <vikramadityapratapsingh98@gmail.com>
2023-09-04 18:49:37 +0530

    add: root endpoint method

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  public helloWorld() {
+    return {
+      status: 200,
+      message: 'The Backend Server Is Ready For Requests!',
+    };
+  }
+}
