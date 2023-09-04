4b26a95701c3a2eb74654c65c0895042818c9e7c
d02ev <vikramadityapratapsingh98@gmail.com>
2023-09-04 19:31:37 +0530

    update: app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/')
+@Controller()
 export class AppController {
   constructor(private readonly _appService: AppService) {}
 
-  @Get('')
+  @Get()
   public hello() {
     return this._appService.helloWorld();
   }
